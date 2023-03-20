d1f12c0a0fc33db6f11c21ef9ca5f08c6025ee45
Victor <miura.hublab@gmail.com>
2023-03-20 18:49:12 -0300

    Api concluida

--- src/main/java/com/miura/booksapi/controllers/AuthorsController.java
+++ src/main/java/com/miura/booksapi/controllers/AuthorsController.java
@@ -2,11 +2,15 @@ package com.miura.booksapi.controllers;
 
 import com.miura.booksapi.models.Author;
 import com.miura.booksapi.models.CreateAuthorRequest;
+import com.miura.booksapi.models.UpdateAuthorRequest;
 import com.miura.booksapi.repositories.AuthorRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/authors")
@@ -24,6 +28,7 @@ public class AuthorsController {
 
     @PostMapping
     public Author createBook (
+
             @RequestBody CreateAuthorRequest request
     ) {
         Author author = new Author(
@@ -32,6 +37,40 @@ public class AuthorsController {
                 Collections.emptyList()
         );
         return authorRepository.save(author);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> updateAuthor (
+            @PathVariable UUID id,
+            @RequestBody UpdateAuthorRequest arequest
+            ) {
+        Optional<Author> existingAuthor = authorRepository.findById(id.toString());
+
+        if (existingAuthor.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+       Author authorToUpdate = existingAuthor.get();
+
+        authorToUpdate.setName(arequest.getName());
+
+        return ResponseEntity.ok(authorRepository.save(authorToUpdate));
+
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> deleteAuthor (
+            @PathVariable UUID id
+    ) {
+        boolean existingAuthor = authorRepository.existsById(id.toString());
+
+        if (!existingAuthor) {
+            return ResponseEntity.notFound().build();
+        }
+
+        authorRepository.deleteById(id.toString());
+
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/miura/booksapi/models/UpdateAuthorRequest.java
+++ src/main/java/com/miura/booksapi/models/UpdateAuthorRequest.java
@@ -4,9 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateAuthorRequest {
     private String authorId;
+
+    private String name;
+
+    private List<Book> books;
 }
